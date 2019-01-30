9c908cac8f62d9f9eea0696677d12baeddacd8a6
Job Albarr <jalbarr@jobs-macbook-pro.local>
2019-01-30 15:20:27 -0500

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Job Albarr on 1/30/19.
+//  Copyright © 2019 Job Albarr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
